4e3fe0d6645525ff4301b6a65141ec01765ebc0a
Yanislav Igonin <yanislavdabest@gmail.com>
2022-10-29 21:28:55 +0300

    clean

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export * from './DarkModeButton';
-export * from './MainCard';
 export * from './GithubIcon';
 export * from './Button';
 export * from './Input';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSidePropsContext, NextPage } from 'next';
 import Head from 'next/head';
-import { DarkModeButton, Layout, MainCard } from '@components';
+import { DarkModeButton, Layout } from '@components';
 import { db } from '@db';
 import { trpc } from '@lib/trpc';
 
@@ -24,29 +24,18 @@ export const getServerSideProps = async ({ req }: GetServerSidePropsContext) => 
 };
 
 const Home: NextPage = () => {
-  const helloQuery = trpc.example.hello.useQuery({ text: 'trpc' });
-  const meQuery = trpc.users.me.useQuery();
-  const me = meQuery.isLoading ? 'loading...' : meQuery.data?.email;
 
   return <Layout>
     <Head>
-      <title>NextJS Template</title>
+      <title>Twittota</title>
     </Head>
 
     <div className="absolute top-0 right-0 p-2">
       <DarkModeButton />
     </div>
 
-    <main className="flex flex-col w-screen h-screen justify-center items-center bg-gradient-to-tr from-emerald-400 to-fuchsia-400 dark:from-emerald-600 dark:to-fuchsia-600">
-      {helloQuery.data
-        ? <h1 className="text-2xl mb-2 text-center dark:text-white">
-          <>{helloQuery.data.message}</> at <>{helloQuery.data.time.toLocaleString()}</>
-        </h1>
-        : <h1 className="text-2xl mb-2 text-center dark:text-white">loading...</h1>}
+    <main className="">
 
-      <h1 className='dark:text-white text-2xl mb-2'>email: {me}</h1>
-
-      <MainCard />
     </main>
   </Layout>;
 };
