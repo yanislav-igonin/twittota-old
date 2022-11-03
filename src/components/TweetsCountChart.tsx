8900cd259857572b6f5c5072a74b04bf96d1cd8b
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-03 02:49:08 +0200

    switch to react-charts

---
+++ src/components/TweetsCountChart.tsx
@@ -0,0 +1,39 @@
+import { useMemo } from 'react';
+import { AxisOptions } from 'react-charts';
+import { types } from 'twitter-api-sdk';
+import dynamic from 'next/dynamic';
+
+const Chart = dynamic(() => import('react-charts').then((mod) => mod.Chart), {
+  ssr: false,
+});
+
+type TweetsCountData = types.components['schemas']['SearchCount'];
+
+export const TweetsCountChart = ({ series, label }: { series: TweetsCountData[], label: string }) => {
+  const data = [{
+    label,
+    data: series,
+    dataType: 'time',
+  }];
+
+  const primaryAxis = useMemo((): AxisOptions<TweetsCountData> => ({
+    getValue: datum => new Date(datum.start) as unknown as Date,
+    scaleType: 'time',
+  }), []);
+  const secondaryAxes = useMemo((): AxisOptions<TweetsCountData>[] => [{
+    getValue: datum => datum.tweet_count as unknown,
+    elementType: 'line',
+  }], []);
+
+  return (
+    <Chart
+      options={{
+        data,
+        // @ts-ignore
+        primaryAxis,
+        // @ts-ignore
+        secondaryAxes,
+      }}
+    />
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,9 @@
 import type { GetServerSidePropsContext, NextPage } from 'next';
 import Head from 'next/head';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import { Layout, Spinner } from '@components';
 import { db } from '@db';
 import { trpc } from '@lib/trpc';
-import { useMemo } from 'react';
-import { AxisOptions } from 'react-charts';
-import { types } from 'twitter-api-sdk';
-import dynamic from 'next/dynamic';
-import { ThemeMode, useDarkMode } from '@lib/hooks/useDarkMode';
-import { useEffect, useState } from 'react';
-
-const Chart = dynamic(() => import('react-charts').then((mod) => mod.Chart), {
-  ssr: false,
-});
+import { TweetsCountChart } from '@components/TweetsCountChart';
 
 const loginRedirect = {
   redirect: {
@@ -34,97 +24,34 @@ export const getServerSideProps = async ({ req }: GetServerSidePropsContext) => 
   return { props: {} };
 };
 
-const darkThemeStroke = 'rgb(248 250 252)';
-const lightThemeStroke = '#8884d8';
-
 const removeDoubleQuotes = (str: string) => str.replace(/"/g, '');
 
-type TweetsCountData = types.components['schemas']['SearchCount'];
-
-const MyChart = ({ series, label }: { series: TweetsCountData[], label: string }) => {
-
-  const data = [
-    {
-      label,
-      data: series,
-      dataType: 'time',
-    },
-  ];
-
-
-  const primaryAxis = useMemo((): AxisOptions<TweetsCountData> => ({
-    getValue: datum => new Date(datum.start),
-    scaleType: 'time',
-  }), []);
-
-  const secondaryAxes = useMemo((): AxisOptions<TweetsCountData>[] => [{
-    getValue: datum => datum.tweet_count,
-    elementType: 'line',
-  }], []);
-
-  return (
-    <Chart
-      options={{
-        data,
-        primaryAxis,
-        secondaryAxes,
-      }}
-    />
-  );
-};
-
-
 const Home: NextPage = () => {
-  const [theme] = useDarkMode();
-  const firstRenderStroke = theme === ThemeMode.Dark ? darkThemeStroke : lightThemeStroke;
-  const [strokeColor, setStrokeColor] = useState(firstRenderStroke);
-
-  useEffect(() => {
-    setStrokeColor(theme === ThemeMode.Dark ? darkThemeStroke : lightThemeStroke);
-  }, [theme]);
-
   const { data } = trpc.trends.getByKeywords.useQuery({});
-  // get window width from brower
+
   return <Layout>
     <Head>
       <title>Trends</title>
     </Head>
 
-    {data
-      ? data.map((trend) => {
-        const { keyword, meta, data: trendData } = trend;
-        if (!trendData) {
-          return null;
-        }
-        return <div key={keyword} className="w-full mb-4">
-          <h1 className='dark:text-slate-50 font-medium text-2xl'>{removeDoubleQuotes(keyword)}</h1>
-          <div className='w-full h-96'>
-            <MyChart series={trendData} label={removeDoubleQuotes(keyword)} />
-          </div>
-          
-          <div className='w-full h-96'>
-            <ResponsiveContainer width="100%" height={400}>
-              <LineChart data={trendData}>
-                <Line type="monotone" dataKey="tweet_count" stroke={strokeColor} strokeWidth={3} dot={false}/>
-                <CartesianGrid stroke="#ccc" />
-                <Tooltip labelFormatter={(v: string) => new Date(v).toLocaleString()} />
-                <XAxis dataKey="start"
-                  angle={20}
-                  interval={24}
-                  tickLine={false}
-                  stroke={strokeColor}
-                  tickFormatter={(v: string) => new Date(v).toLocaleDateString()} />
-                <YAxis 
-                dataKey="tweet_count" stroke={strokeColor} />
-              </LineChart>
-            </ResponsiveContainer>
-          </div>
-          <h1 className='dark:text-slate-50 font-medium text-2xl'>Total tweets count for period: {meta?.total_tweet_count}</h1>
-        </div>;
-      })
-      : <div className='w-screen h-screen flex justify-center items-center'>
-        <Spinner width={40} height={40} />
-      </div>}
+    <div className="flex items-center justify-center flex-wrap">
+      {data
+        ? data.map((trend) => {
+          const { keyword, data: trendData } = trend;
+          if (!trendData) {
+            return null;
+          }
+          return <div key={keyword} className="w-full mb-4">
+            <h1 className='dark:text-slate-50 font-medium text-2xl'>{removeDoubleQuotes(keyword)}</h1>
+            <div className='w-full h-96'>
+              <TweetsCountChart series={trendData} label={removeDoubleQuotes(keyword)} />
+            </div>
+          </div>;
+        })
+        : <div className='w-screen h-screen flex justify-center items-center'>
+          <Spinner width={40} height={40} />
+        </div>}
+    </div>
   </Layout>;
 };
 
