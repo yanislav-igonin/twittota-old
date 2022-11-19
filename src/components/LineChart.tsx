5b53f4b513e7ae40eb95c96eb16ef24595c904de
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-19 12:54:39 +0200

    update chart styling

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -1,20 +1,18 @@
 import { ThemeMode, useDarkMode } from '@lib/hooks/useDarkMode';
 import { FC, useEffect, useState } from 'react';
-import { LineChart as LineChartLib, Line, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
+import { LineChart as LineChartLib, Line, CartesianGrid, Tooltip, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 
 const darkThemeStroke = 'rgb(248 250 252)';
 const lightThemeStroke = '#8884d8';
 
 type Props = {
-  data: any[];
   xKey: string;
   yKey: string;
+  // Data should contain and be sorted by xKey.
+  data: any[];
 }
-
 export const LineChart: FC<Props> = ({ data, xKey, yKey }) => {
   const [theme] = useDarkMode();
-  // get window width from brower
-  const width = typeof window !== 'undefined' ? window.innerWidth - 48 - 16 - 16 : 1000;
   const firstRenderStroke = theme === ThemeMode.Dark ? darkThemeStroke : lightThemeStroke;
   const [strokeColor, setStrokeColor] = useState(firstRenderStroke);
 
@@ -22,15 +20,18 @@ export const LineChart: FC<Props> = ({ data, xKey, yKey }) => {
     setStrokeColor(theme === ThemeMode.Dark ? darkThemeStroke : lightThemeStroke);
   }, [theme]);
 
-  return <LineChartLib width={width} height={400} data={data}>
-    <Line type="monotone" dataKey={yKey} stroke={strokeColor} />
-    <CartesianGrid stroke="#ccc" />
-    <Tooltip labelFormatter={(v: string) => new Date(v).toLocaleString()} />
-    <XAxis dataKey={xKey}
-      angle={20}
-      interval={24}
-      stroke={strokeColor}
-      tickFormatter={(v: string) => new Date(v).toLocaleDateString()} />
-    <YAxis dataKey={yKey} stroke={strokeColor} />
-  </LineChartLib>;
+  return <ResponsiveContainer width="100%" height={400}>
+    <LineChartLib data={data}>
+      <Line type="monotone" dataKey={yKey} stroke={strokeColor} strokeWidth={2} />
+      <CartesianGrid stroke="#ccc" />
+      <Tooltip labelFormatter={(v: string) => new Date(v).toLocaleString()} />
+      <XAxis dataKey={xKey}
+        strokeWidth={2}
+        angle={20}
+        interval={24}
+        stroke={strokeColor}
+        tickFormatter={(v: string) => new Date(v).toLocaleDateString()} />
+      <YAxis dataKey={yKey} stroke={strokeColor} strokeWidth={2} />
+    </LineChartLib>
+  </ResponsiveContainer>;
 };
